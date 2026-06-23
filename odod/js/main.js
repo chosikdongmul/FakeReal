0ef0fc6e50800e27a575553fd07b1e19d693e02c
chosikdongmul <snapquest101@gmail.com>
2026-06-24 01:23:37 +0900

    Update main.js

--- odod/js/main.js
+++ odod/js/main.js
@@ -534,11 +534,6 @@ const DEFAULT_DATA = {
   ],
   "banners": [
     {
-      "image": "",
-      "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/hL2AHOehBy4?si=q3pFUyFwfg5X1aRK\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
-      "caption": ""
-    },
-    {
       "image": "assets/images/bannertest.png",
       "video": "",
       "caption": ""
